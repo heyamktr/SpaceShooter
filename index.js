7357fe507132329e47c74e3bae291208971c569d
Truong Nguyen Tien Khanh <88968349+khanhtruongloveu@users.noreply.github.com>
2025-01-04 01:59:34 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,211 @@
+const canvas = document.querySelector('canvas')
+
+const c= canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height= innerHeight
+
+class Player{
+    constructor(){
+        
+        this.velocity={
+            x:0,
+            y:0
+        }
+        this.rotation=0
+
+
+        const image = new Image()
+        image.src='./player.png'
+        image.onload= ()=>{
+            const scale=0.15
+            this.image=image
+            this.width=image.width*scale
+            this.height=image.height*scale
+            this.position={
+                x: canvas.width/2 - this.width/2,
+                y:canvas.height- this.height -20
+            }
+        }
+    }
+    draw(){
+       // c.fillStyle ='red'
+       // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        c.save()
+        c.translate(player.position.x+player.width/2, 
+            player.position.y+player.height/2)
+        c.rotate(this.rotation)
+        c.translate(-player.position.x-player.width/2, 
+            -player.position.y-player.height/2)
+         c.drawImage(
+            this.image,
+            this.position.x, 
+            this.position.y,
+            this.width,
+            this.height
+    )
+    c.restore()
+    }
+
+
+    update(){
+        if(this.image){
+        this.draw()
+        this.position.x += this.velocity.x
+        }
+    }
+}
+class Projectile{
+    constructor({position, velocity}){
+        this.position= position;
+        this.velocity= velocity;
+        this.radius=3;
+    }
+    draw(){
+        c.beginPath()
+        c.arc(this.position.x, this.position.y,this.radius,0,
+            Math.PI*2
+        )
+        c.fillStyle='red'
+        c.fill()
+        c.closePath()
+    }
+    update(){
+        this.draw()
+        this.position.x+=this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
+//INVADERS
+class Invader {
+    constructor(canvas, c) {
+        this.canvas = canvas; // Reference to the canvas
+        this.c = c; // Canvas rendering context
+        this.velocity = {
+            x: 0,
+            y: 0,
+        };
+
+        this.image = new Image();
+        this.image.src = './enemy.png';
+
+        this.image.onload = () => {
+            const scale = 0.03;
+            this.width = this.image.width * scale;
+            this.height = this.image.height * scale;
+            this.position = {
+                x: canvas.width / 2 - this.width / 2,
+                y: canvas.height / 3,
+            };
+        };
+    }
+
+    draw() {
+        if (this.image && this.position) {
+            this.c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+        }
+    }
+
+    update() {
+        if (this.image && this.position) {
+            this.draw();
+            this.position.x += this.velocity.x;
+            this.position.y += this.velocity.y;
+        }
+    }
+}
+
+
+
+
+
+const player = new Player()
+const projectiles=[]
+const invader = new Invader(canvas,c)
+const keys={
+    a:{
+        pressed:false
+    },
+    d:{
+        pressed:false
+    },
+    space:{
+        pressed:false
+    }
+}
+function animate(){
+    requestAnimationFrame(animate);
+    c.fillStyle = 'black';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    invader.update();
+    player.update();
+    projectiles.forEach((projectile, index) => {
+        if (projectile.position.y + projectile.radius <= 0) {
+            setTimeout(() => {
+                projectiles.splice(index, 1); // Remove the projectile safely
+            }, 0);
+        } else {
+            projectile.update();
+        }
+    });
+
+    if (keys.a.pressed && player.position.x >= 0) {
+        player.velocity.x = -5; // Move left
+        player.rotation = -0.15;
+    } else if (keys.d.pressed && player.position.x + player.width <= canvas.width) {
+        player.velocity.x = 5; // Move right
+        player.rotation = 0.15;
+    } else {
+        player.velocity.x = 0; // Stop moving
+        player.rotation = 0;
+    }
+}
+animate()
+
+addEventListener('keydown',({key})=>{
+    switch(key){
+        case 'a':
+            console.log('left')
+            
+            keys.a.pressed= true
+            break
+        case 'd':
+            console.log('right')
+            keys.d.pressed= true
+            break
+        case ' ':
+            console.log('space')
+            projectiles.push(
+                new Projectile({
+                position:{
+                    x:player.position.x+player.width/2,
+                    y:player.position.y
+                },
+                velocity:{
+                    x:0,
+                    y:-12
+                }
+            }
+            
+            ))
+          //  console.log(projectiles)
+            break
+    }
+})
+addEventListener('keyup',({key})=>{
+    switch(key){
+        case 'a':
+         //   console.log('left')
+            
+            keys.a.pressed= false
+            break
+        case 'd':
+          //  console.log('right')
+            keys.d.pressed= false
+            break
+        case ' ':
+          //  console.log('space')
+            break
+    }
+})
